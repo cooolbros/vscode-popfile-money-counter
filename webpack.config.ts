dd02477d11ad039426e643927bb35d928de314cd
cooolbros <pfwobcke@gmail.com>
2021-10-14 00:10:48 +1100

    Compile with webpack

---
+++ webpack.config.ts
@@ -0,0 +1,26 @@
+// https://code.visualstudio.com/api/extension-guides/web-extensions
+export default {
+	mode: "none",
+	target: "webworker",
+	entry: {
+		extension: "./src/extension.ts"
+	},
+	output: {
+		filename: "[name].js"
+	},
+	resolve: {
+		mainFields: ["browser", "module", "main"],
+		extensions: [".ts", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts/,
+				use: "ts-loader"
+			}
+		]
+	},
+	externals: {
+		vscode: "commonjs vscode"
+	}
+}
